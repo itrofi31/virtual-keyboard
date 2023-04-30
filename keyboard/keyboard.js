6afbe4283a1b8f171aeced8ea1fb90588116519a
Ivan Trofimov <triv99@mail.ru>
2023-04-30 13:57:37 +0300

    refactor: add functions for DRY concept

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -16,6 +16,7 @@ const Keyboard = {
   properties: {
     value: '',
     capsLock: false,
+    event: null,
   },
 
   init() {
@@ -60,12 +61,12 @@ const Keyboard = {
       '`','1','2','3','4','5','6','7','8','9','0','-','=','del',
       'tab','q','w','e','r','t','y','u','i','o','p','[',']','\\',
       'caps lock','a','s','d','f','g','h','j','k','l',';',"'",'enter',
-      'shift','z','x','c','v','b','n','m',',','.','/','shift-r',
-      'fn','ctrl','opt','cmd','space','cmd','opt',];
+      'shift-1','z','x','c','v','b','n','m',',','.','/','shift-2',
+      'ctrl','opt-1','cmd-1','space','cmd-2','opt-2',];
 
     keyLayout.forEach((key) => {
       const keyElement = document.createElement('button');
-      const insertLineBreak = ['del', '\\', 'enter', 'shift-r'].includes(key);
+      const insertLineBreak = ['del', '\\', 'enter', 'shift-2'].includes(key);
 
       //Add classes/attributes
       keyElement.setAttribute('type', 'button');
@@ -73,8 +74,9 @@ const Keyboard = {
 
       switch (key) {
         case 'del':
-          keyElement.classList.add('keyboard__key--wide');
+          keyElement.classList.add('keyboard__key--medium');
           keyElement.textContent = key;
+          keyElement.dataset.value = 'backspace';
 
           keyElement.addEventListener('click', function () {
             Keyboard.deleteChar();
@@ -83,15 +85,16 @@ const Keyboard = {
           break;
 
         case 'tab':
-          keyElement.classList.add('keyboard__key--wide');
+          keyElement.classList.add('keyboard__key--medium');
           keyElement.textContent = key;
+          keyElement.dataset.value = 'tab';
 
           break;
 
         case 'caps lock':
           keyElement.classList.add('keyboard__key--wide');
           keyElement.textContent = key;
-          keyElement.dataset.value = 'caps lock';
+          keyElement.dataset.value = 'capslock';
 
           keyElement.addEventListener('click', function () {
             Keyboard._toggleCapsLock();
@@ -105,7 +108,7 @@ const Keyboard = {
         case 'enter':
           keyElement.classList.add('keyboard__key--wide');
           keyElement.textContent = 'enter';
-          keyElement.dataset.value = 'Enter';
+          keyElement.dataset.value = 'enter';
 
           keyElement.addEventListener('click', function () {
             Keyboard.properties.value += '\n';
@@ -123,21 +126,16 @@ const Keyboard = {
           });
           break;
 
-        case 'shift':
-          keyElement.classList.add('keyboard__key--medium');
+        case 'shift-1':
+          keyElement.classList.add('keyboard__key--wide');
           keyElement.textContent = 'shift';
-          keyElement.dataset.value = 'shift';
+          keyElement.dataset.value = 'shift-1';
           break;
 
-        case 'shift-r':
-          keyElement.classList.add('keyboard__key--medium');
+        case 'shift-2':
+          keyElement.classList.add('keyboard__key--wide');
           keyElement.textContent = 'shift';
-          keyElement.dataset.value = 'shift-r';
-          break;
-
-        case '^':
-          keyElement.classList.add('keyboard__key--margin-left');
-          keyElement.textContent = '^';
+          keyElement.dataset.value = 'shift-2';
           break;
 
         case 'space':
@@ -150,6 +148,41 @@ const Keyboard = {
 
             Keyboard._triggerEvent('oninput');
           });
+          break;
+
+        case 'cmd-1':
+          keyElement.classList.add('keyboard__key--medium');
+          keyElement.dataset.value = 'meta-1';
+          keyElement.textContent = 'cmd';
+          break;
+
+        case 'cmd-2':
+          keyElement.classList.add('keyboard__key--medium');
+          keyElement.dataset.value = 'meta-2';
+          keyElement.textContent = 'cmd';
+          break;
+
+        case 'opt-1':
+          keyElement.classList.add('keyboard__key--medium');
+          keyElement.dataset.value = 'alt-1';
+          keyElement.textContent = 'opt';
+          break;
+
+        case 'opt-2':
+          keyElement.classList.add('keyboard__key--medium');
+          keyElement.dataset.value = 'alt-2';
+          keyElement.textContent = 'opt';
+          break;
+
+        case 'ctrl':
+          keyElement.classList.add('keyboard__key--medium');
+          keyElement.dataset.value = 'control';
+          keyElement.textContent = 'ctrl';
+          break;
+
+        case '\\':
+          keyElement.dataset.value = '\\\\';
+          keyElement.textContent = '\\';
           break;
 
         default:
@@ -209,56 +242,122 @@ const Keyboard = {
       0,
       Keyboard.properties.value.length - 1
     );
+    //check if textarea is out of focus, so do not delete twice
+    if (Keyboard.elements.textArea !== document.activeElement) {
+      Keyboard.elements.textArea.value = Keyboard.properties.value;
+    }
+  },
+
+  _toggleActive(action) {
+    document
+      .querySelector(
+        `.keyboard__key[data-value='${Keyboard.properties.event.key.toLowerCase()}']`
+      )
+      ?.classList[action === 'add' ? 'add' : 'remove']('keyboard__key__active');
+  },
+
+  _toggleActiveWithLocation(action) {
+    document
+      .querySelector(
+        `.keyboard__key[data-value='${Keyboard.properties.event.key.toLowerCase()}-${
+          Keyboard.properties.event.location
+        }']`
+      )
+      ?.classList[action === 'add' ? 'add' : 'remove']('keyboard__key__active');
   },
 
   showPressedKeys() {
     document.addEventListener('keydown', (e) => {
       //prettier-ignore
-      const specialSymbols = ['Shift','Control','Alt','Meta','Tab','Enter','Space','Backspace',];
-      console.log(e.key);
-      if (
-        // document.querySelector(
-        //   `.keyboard__key[data-value='${e.key.toLowerCase()}']`
-        // )
-        !specialSymbols.includes(e.key)
-      ) {
+      const specialSymbols = ['Shift','Control','Alt','Meta','Tab','Enter','Space','Backspace','CapsLock'];
+
+      if (!specialSymbols.includes(e.key)) {
         Keyboard.properties.value += e.key;
-        Keyboard.elements.textArea.value = Keyboard.properties.value;
+        if (Keyboard.elements.textArea !== document.activeElement) {
+          Keyboard.elements.textArea.value = Keyboard.properties.value;
+        }
       }
-
-      document
-        .querySelector(`.keyboard__key[data-value='${e.key.toLowerCase()}']`)
-        ?.classList.add('active');
-
+      console.log(e);
+      Keyboard.properties.event = e;
       switch (e.key) {
         case 'CapsLock':
-          document
-            .querySelector(`.keyboard__key[data-value='caps lock']`)
-            .classList.add('keyboard__key__active');
-
+          Keyboard._toggleActive('add');
           Keyboard._toggleCapsLock();
           break;
+
         case 'Backspace':
+          Keyboard._toggleActive('add');
           Keyboard.deleteChar();
           break;
+
+        case 'Tab':
+          Keyboard._toggleActive('add');
+          break;
+
         case 'Enter':
+          Keyboard._toggleActive('add');
           Keyboard.properties.value += '\n';
+          break;
+
+        case 'Shift':
+          Keyboard._toggleActiveWithLocation('add');
+          break;
+
+        case 'Meta':
+          Keyboard._toggleActiveWithLocation('add');
+          break;
+
+        case 'Alt':
+          Keyboard._toggleActiveWithLocation('add');
+          break;
+
+        case 'Control':
+          Keyboard._toggleActive('add');
+          break;
+
+        default:
+          Keyboard._toggleActive('add');
           break;
       }
     });
 
     document.addEventListener('keyup', (e) => {
-      document
-        .querySelector(`.keyboard__key[data-value='${e.key.toLowerCase()}']`)
-        ?.classList.remove('active');
       switch (e.key) {
         case 'CapsLock':
-          document
-            .querySelector(`.keyboard__key[data-value='caps lock']`)
-            .classList.remove('keyboard__key__active');
-
+          Keyboard._toggleActive('remove');
           Keyboard._toggleCapsLock();
+          break;
 
+        case 'Backspace':
+          Keyboard._toggleActive('remove');
+          break;
+
+        case 'Tab':
+          Keyboard._toggleActive('remove');
+          break;
+
+        case 'Enter':
+          Keyboard._toggleActive('remove');
+          break;
+
+        case 'Shift':
+          Keyboard._toggleActiveWithLocation('remove');
+          break;
+
+        case 'Meta':
+          Keyboard._toggleActiveWithLocation('remove');
+          break;
+
+        case 'Alt':
+          Keyboard._toggleActiveWithLocation('remove');
+          break;
+
+        case 'Control':
+          Keyboard._toggleActive('remove');
+          break;
+
+        default:
+          Keyboard._toggleActive('remove');
           break;
       }
     });
